4f01bf6eacae75637845101857c863f69c599010
edgeone pages <simxin@tencent.com>
2026-08-12 19:53:42 +0800

    update meta info

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>EdgeOne Makers Template</title>
+        <meta name="description" content="Demo only · EdgeOne Makers" />
+        <meta name="keywords" content="EdgeOne Makers, Demo only" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
